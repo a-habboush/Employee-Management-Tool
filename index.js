69531c81800d24b3da20701b4d6d7dbdda73eddc
Abdulrahman Habboush <a.habboush04@gmail.com>
2024-09-03 20:36:44 +0300

    home page mostly done

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-let a = document.querySelector(".navbar-toggler");
+
+const searchbar = document.getElementById('searchBar');
+const vacReqCards = document.getElementById('cardList');
+const reqCards = document.querySelectorAll('.vacationReqBody .col');
+const empName = document.querySelectorAll('.vacationReqBody .col h4');
 
 
-a.addEventListener('click', ()=> {
-    alert('aaaaa');
+
+
+
+
+
+searchbar.addEventListener('input', e => {
+const inputValue = e.target.value.toLowerCase();
+
+
+reqCards.forEach(card => {
+
+    const isVisible = card.querySelector("h4").textContent.toLowerCase().includes(inputValue);
+    card.classList.toggle("hide", !isVisible)
+ 
+ })
 
 })
+
+    
+    
+
+
+    
