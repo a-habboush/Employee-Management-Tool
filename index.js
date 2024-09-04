9af4628a0e9108775ef670f36863311d41a665e1
Abdulrahman Habboush <a.habboush04@gmail.com>
2024-09-04 21:17:32 +0300

    fixed a ton of css on home page and pretty much finished profile page

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 const searchbar = document.getElementById('searchBar');
-const vacReqCards = document.getElementById('cardList');
 const reqCards = document.querySelectorAll('.vacationReqBody .col');
-const empName = document.querySelectorAll('.vacationReqBody .col h4');
+
 
 
 
@@ -16,7 +15,7 @@ const inputValue = e.target.value.toLowerCase();
 
 reqCards.forEach(card => {
 
-    const isVisible = card.querySelector("h4").textContent.toLowerCase().includes(inputValue);
+    const isVisible = card.querySelector("h5").textContent.toLowerCase().includes(inputValue);
     card.classList.toggle("hide", !isVisible)
  
  })
